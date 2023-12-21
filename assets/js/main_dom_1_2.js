12ec8f85403aad5e0b19f810fb7e429f1eacc306
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-21 14:32:47 +0100

    dom 1_2

--- assets/js/main_dom_1_2.js
+++ assets/js/main_dom_1_2.js
@@ -1,9 +1,17 @@
+let counter = 1
 let myFunction = () => {
     let bgColorChange = document.getElementsByClassName('example')
 
     for(let i = 0; i < bgColorChange.length; i++) {
-        bgColorChange[i].style.backgroundColor = 'black'
-        bgColorChange[i].style.color = 'white'
+        
+        if (counter % 2 == 0) {
+            bgColorChange[i].style.backgroundColor = 'black'
+            bgColorChange[i].style.color = 'white'
+        } else {
+            bgColorChange[i].style.backgroundColor = ''
+            bgColorChange[i].style.Color = ''
+        }
+    }
+    counter += 1
 }
 
-}
