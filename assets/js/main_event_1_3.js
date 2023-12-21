472d7c597574f494b10fbd3f9e76d6a8e4179f27
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-21 19:36:37 +0100

    event 1_3

--- assets/js/main_event_1_3.js
+++ assets/js/main_event_1_3.js
@@ -1,17 +1,7 @@
-const card = document.getElementsByClassName("card")
-// let cardArray = Array.from(card)
-console.log(card);
-
-// let firstChild = body.firstElementChild
-// let secondChild = body.firstElementChild.nextElementSibling
-// let thirdChild = body.firstElementChild.nextElementSibling.nextElementSibling
-// let fourthChild = body.lastElementChild.previousElementSibling
-// let fivesChild = body.lastElementChild
-
 let changeColor = () => {
-    for (let i = 0; i < card.length; i++) {
-        card[i].addEventListener ("click", () => {
-            card.style.backgroundColor = "black"
-        })
-    }
+    let card = document.getElementsByClassName("card")
+    let cardArray = Array.from(card)
+    let cardIndex = cardArray.indexOf(event.target)
+    cardArray[cardIndex].style.backgroundColor = "black"
+    cardArray[cardIndex].style.color = "white"
 }
